5699192ba84afd624b95107c045a741c82aafdca
mrxu007 <3202108909@qq.com>
2019-11-14 19:08:02 +0800

    no message

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const http = require('http')
+const http = require('https')
 const shell = require('shelljs')
 const createHandler = require('github-webhook-handler')
 const handler = createHandler({ path: '/webhook', secret: 'vnshop' })
